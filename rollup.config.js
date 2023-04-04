546a2ae3bc4fe27932df0459e152d8df042bbd21
Tomas Rezac <rezi99@gmail.com>
2023-04-04 09:50:31 +0200

    rollup config update

--- rollup.config.js
+++ rollup.config.js
@@ -1,27 +1,36 @@
-import babel from '@rollup/plugin-babel';
-import resolve from '@rollup/plugin-node-resolve';
+import { babel, getBabelOutputPlugin } from '@rollup/plugin-babel';
+
 import pkg from './package.json';
+import resolve from '@rollup/plugin-node-resolve';
 
 const extensions = ['.js', '.ts'];
 
 export default [
   {
     input: 'src/index.ts',
+    plugins: [
+      resolve({
+        extensions,
+      }),
+      babel({
+        extensions,
+        exclude: 'node_modules/**',
+      }),
+    ],
     output: [
       {
         file: pkg.main,
         format: 'cjs',
+        plugins: [
+          getBabelOutputPlugin({
+            plugins: ['@babel/plugin-proposal-optional-chaining'],
+          }),
+        ],
       },
       {
         file: pkg.module,
         format: 'es',
       },
-    ],
-    plugins: [
-      resolve({
-        extensions,
-      }),
-      babel({ extensions, exclude: 'node_modules/**' }),
     ],
   },
 ];
